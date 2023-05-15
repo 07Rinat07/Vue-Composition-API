a54923314b3d23a44f519a08c5b0e0acc44840df
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-15 13:55:56 +0500

    Header and nav components_6_less

---
+++ src/components/TheHeaderProgress.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template>
+  <a href="#" class="text-sm">
+    <div v-if="false" class="flex items-center gap-1">
+      Day complete!
+      <CheckCircleIcon class="text-green-500 h-7" />
+    </div>
+    <div v-else class="flex items-center gap-1">
+      <div>Progress: <span class="font-mono">20%</span></div>
+      <div class="w-3 h-3 bg-red-500 rounded-full"></div>
+    </div>
+  </a>
+</template>
