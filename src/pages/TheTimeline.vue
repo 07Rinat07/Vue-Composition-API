74c8b0a0b2077889b10e2c4aeb4509757f6e815a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-15 17:29:57 +0500

    Generate timeline hours_14_less

--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,16 +1,21 @@
+<script setup>
+import { generateTimelineItems } from '../functions'
+const timelineItems = generateTimelineItems()
+</script>
+
 <template>
   <div class="mt-7">
     <ul>
       <li
-        v-for="i in [0, 1, 2, 3, 4, 5]"
-        :key="i"
+        v-for="{ hour } in timelineItems"
+        :key="hour"
         class="relative flex flex-col gap-2 px-4 py-10 border-t border-gray-200"
       >
         <a
           href="#"
           class="absolute px-2 font-mono text-lg text-gray-500 -translate-x-1/2 bg-gray-100 rounded -top-4 left-1/2"
         >
-          {{ i }}:00
+          {{ hour }}:00
         </a>
       </li>
     </ul>
