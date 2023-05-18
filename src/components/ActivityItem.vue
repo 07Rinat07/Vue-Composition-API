5bd700b49b63647fb159932cdb2f615ed0551f31
Rinat Sarmuldin <ura07srr@gmail.com>
2023-05-18 20:23:16 +0500

    Use normal script inside components_30_les

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,37 +1,28 @@
+<script>
+import { BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '../constants'
+const typeClasses = {
+  [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
+  [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200'
+}
+</script>
+
 <script setup>
-import { ref } from 'vue'
-import { TrashIcon } from '@heroicons/vue/24/outline'
-import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '../constants'
-import { isActivityValid } from '../validators'
-import BaseButton from './BaseButton.vue'
-import BaseSelect from './BaseSelect.vue'
+import { isButtonTypeValid } from '../validators'
 defineProps({
-  activity: {
-    required: true,
+  type: {
+    default: BUTTON_TYPE_NEUTRAL,
     type: String,
-    validator: isActivityValid
+    validator: isButtonTypeValid
   }
 })
 
-const secondsToComplete = ref(null)
+
 </script>
 
 <template>
-  <li class="flex flex-col gap-2 p-4">
-    <div class="flex items-center gap-2">
-      <BaseButton :type="BUTTON_TYPE_DANGER">
-        <TrashIcon class="h-8" />
-      </BaseButton>
-      <span class="text-xl truncate">{{ activity }}</span>
-    </div>
-    <div>
-      <BaseSelect
-        class="font-mono"
-        placeholder="h:mm"
-        :options="PERIOD_SELECT_OPTIONS"
-        :selected="secondsToComplete"
-        @select="secondsToComplete = $event"
-      />
-    </div>
-  </li>
+  <button
+    :class="`${typeClasses[type]} rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
+  >
+    <slot></slot>
+  </button>
 </template>
