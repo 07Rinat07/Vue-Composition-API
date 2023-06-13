3b013fe7714ecbc708838bc3b8f7c6a2c6d3446a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-13 10:12:49 +0500

    Improve period select options generation_48les

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,22 +1,21 @@
 <script setup>
 import { TrashIcon } from '@heroicons/vue/24/outline'
-import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '@/constants'
-import { isActivityValid, isUndefined, isNumber } from '@/validators'
+import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '../constants'
+import { isActivityValid, isUndefined, isNumber } from '../validators'
 import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
-  defineProps({
-                activity: {
-                  required: true,
-                  type: Object,
-                  validator: isActivityValid
-                }
-              })
-  const emit = defineEmits({
-    setSecondsToComplete: isNumber,
-    delete: isUndefined
-  })
+defineProps({
+  activity: {
+    required: true,
+    type: Object,
+    validator: isActivityValid
+  }
+})
+const emit = defineEmits({
+  setSecondsToComplete: isNumber,
+  delete: isUndefined
+})
 </script>
-
 <template>
   <li class="flex flex-col gap-2 p-4">
     <div class="flex items-center gap-2">
@@ -28,7 +27,7 @@ import BaseSelect from './BaseSelect.vue'
     <div>
       <BaseSelect
           class="font-mono"
-          placeholder="h:mm"
+          placeholder="hh:mm"
           :options="PERIOD_SELECT_OPTIONS"
           :selected="activity.secondsToComplete || null"
           @select="emit('setSecondsToComplete', $event || 0)"
--- src/functions.js
+++ src/functions.js
@@ -1,42 +1,58 @@
 import {
   PAGE_TIMELINE,
   SECONDS_IN_HOUR,
+  SECONDS_IN_MINUTE,
+  MINUTES_IN_HOUR,
   HOURS_IN_DAY,
   MIDNIGHT_HOUR
 } from './constants'
-import { isPageValid, isNull } from './validators'
-export function normalizePageHash() {
-  const page = window.location.hash.slice(1)
-  if (isPageValid(page)) {
-    return page
-  }
-  window.location.hash = PAGE_TIMELINE
-  return PAGE_TIMELINE
-}
-export function normalizeSelectValue(value) {
-  return isNull(value) || isNaN(value) ? value : +value
-}
-export function generateActivities() {
-  return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
-    id: id(),
-    name,
-    secondsToComplete: hours * SECONDS_IN_HOUR
-  }))
-}
-export function id() {
-  return Date.now().toString(36) + Math.random().toString(36).substring(2)
-}
-export function generateTimelineItems() {
-  const timelineItems = []
 
-  for (let hour = MIDNIGHT_HOUR; hour < HOURS_IN_DAY; hour++) {
-    timelineItems.push({
-      hour,
-      activityId: null
-    })
+  import { isPageValid, isNull } from './validators'
+  export function normalizePageHash() {
+    const page = window.location.hash.slice(1)
+    if (isPageValid(page)) {
+      return page
+    }
+    window.location.hash = PAGE_TIMELINE
+    return PAGE_TIMELINE
   }
-  return timelineItems
-}
-export function generateActivitySelectOptions(activities) {
-  return activities.map((activity) => ({ value: activity.id, label: activity.name }))
-}
+  export function normalizeSelectValue(value) {
+    return isNull(value) || isNaN(value) ? value : +value
+  }
+  export function generateActivities() {
+    return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
+      id: id(),
+      name,
+      secondsToComplete: hours * SECONDS_IN_HOUR
+    }))
+  }
+  export function id() {
+    return Date.now().toString(36) + Math.random().toString(36).substring(2)
+  }
+  export function generateTimelineItems() {
+    const timelineItems = []
+    for (let hour = MIDNIGHT_HOUR; hour < HOURS_IN_DAY; hour++) {
+      timelineItems.push({
+        hour,
+        activityId: null
+      })
+    }
+    return timelineItems
+  }
+  export function generateActivitySelectOptions(activities) {
+    return activities.map((activity) => ({ value: activity.id, label: activity.name }))
+  }
+
+  export function generatePeriodSelectOptions(periodsInMinutes) {
+    return periodsInMinutes.map((periodInMinutes) => ({
+      value: periodInMinutes * SECONDS_IN_MINUTE,
+      label: generatePeriodSelectOptionsLabel(periodInMinutes)
+    }))
+  }
+
+  function generatePeriodSelectOptionsLabel(periodInMinutes) {
+    const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR).toString().padStart(2, 0)
+    const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
+
+    return `${hours}:${minutes}`
+  }
