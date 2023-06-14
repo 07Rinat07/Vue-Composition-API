0bfd8a485e97fd990a9825c15cfcf788b2886137
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-14 17:44:29 +0500

    Stopwatch component_50_les

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,10 @@
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
 import { generatePeriodSelectOptions } from './functions'
-
 export const BUTTON_TYPE_PRIMARY = 'primary'
 export const BUTTON_TYPE_SUCCESS = 'success'
 export const BUTTON_TYPE_WARNING = 'warning'
 export const BUTTON_TYPE_NEUTRAL = 'neutral'
 export const BUTTON_TYPE_DANGER = 'danger'
-
 export const BUTTON_TYPES = [
   BUTTON_TYPE_PRIMARY,
   BUTTON_TYPE_SUCCESS,
@@ -17,6 +15,8 @@ export const BUTTON_TYPES = [
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
+
+export const MILLISECONDS_IN_SECOND = 1000
 export const SECONDS_IN_MINUTE = 60
 export const MINUTES_IN_HOUR = 60
 export const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
--- src/functions.js
+++ src/functions.js
@@ -1,58 +1,67 @@
 import {
-  PAGE_TIMELINE,
-  SECONDS_IN_HOUR,
-  SECONDS_IN_MINUTE,
-  MINUTES_IN_HOUR,
-  HOURS_IN_DAY,
-  MIDNIGHT_HOUR
+    PAGE_TIMELINE,
+    MILLISECONDS_IN_SECOND,
+    SECONDS_IN_HOUR,
+    SECONDS_IN_MINUTE,
+    MINUTES_IN_HOUR,
+    HOURS_IN_DAY,
+    MIDNIGHT_HOUR
 } from './constants'
+import { isPageValid, isNull } from './validators'
 
-  import { isPageValid, isNull } from './validators'
-  export function normalizePageHash() {
+export function formatSeconds(seconds) {
+    const date = new Date()
+
+    date.setTime(Math.abs(seconds) * MILLISECONDS_IN_SECOND)
+
+    const utc = date.toUTCString()
+
+    return utc.substring(utc.indexOf(':') - 2, utc.indexOf(':') + 6)
+}
+
+export function normalizePageHash() {
     const page = window.location.hash.slice(1)
-    if (isPageValid(page)) {
-      return page
+        if (isPageValid(page)) {
+            return page
+        }
+        window.location.hash = PAGE_TIMELINE
+        return PAGE_TIMELINE
     }
-    window.location.hash = PAGE_TIMELINE
-    return PAGE_TIMELINE
-  }
-  export function normalizeSelectValue(value) {
-    return isNull(value) || isNaN(value) ? value : +value
-  }
-  export function generateActivities() {
-    return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
-      id: id(),
-      name,
-      secondsToComplete: hours * SECONDS_IN_HOUR
-    }))
-  }
-  export function id() {
-    return Date.now().toString(36) + Math.random().toString(36).substring(2)
-  }
-  export function generateTimelineItems() {
-    const timelineItems = []
-    for (let hour = MIDNIGHT_HOUR; hour < HOURS_IN_DAY; hour++) {
-      timelineItems.push({
-        hour,
-        activityId: null
-      })
+    export function normalizeSelectValue(value) {
+        return isNull(value) || isNaN(value) ? value : +value
     }
-    return timelineItems
-  }
-  export function generateActivitySelectOptions(activities) {
-    return activities.map((activity) => ({ value: activity.id, label: activity.name }))
-  }
-
-  export function generatePeriodSelectOptions(periodsInMinutes) {
-    return periodsInMinutes.map((periodInMinutes) => ({
-      value: periodInMinutes * SECONDS_IN_MINUTE,
-      label: generatePeriodSelectOptionsLabel(periodInMinutes)
-    }))
-  }
-
-  function generatePeriodSelectOptionsLabel(periodInMinutes) {
-    const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR).toString().padStart(2, 0)
-    const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
-
-    return `${hours}:${minutes}`
-  }
+    export function generateActivities() {
+        return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
+            id: id(),
+            name,
+            secondsToComplete: hours * SECONDS_IN_HOUR
+        }))
+    }
+    export function id() {
+        return Date.now().toString(36) + Math.random().toString(36).substring(2)
+    }
+    export function generateTimelineItems() {
+        const timelineItems = []
+        for (let hour = MIDNIGHT_HOUR; hour < HOURS_IN_DAY; hour++) {
+            timelineItems.push({
+                hour,
+                activityId: null,
+                activitySeconds: 0
+            })
+        }
+        return timelineItems
+    }
+    export function generateActivitySelectOptions(activities) {
+        return activities.map((activity) => ({ value: activity.id, label: activity.name }))
+    }
+    export function generatePeriodSelectOptions(periodsInMinutes) {
+        return periodsInMinutes.map((periodInMinutes) => ({
+            value: periodInMinutes * SECONDS_IN_MINUTE,
+            label: generatePeriodSelectOptionsLabel(periodInMinutes)
+        }))
+    }
+    function generatePeriodSelectOptionsLabel(periodInMinutes) {
+        const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR).toString().padStart(2, 0)
+        const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
+        return `${hours}:${minutes}`
+    }
