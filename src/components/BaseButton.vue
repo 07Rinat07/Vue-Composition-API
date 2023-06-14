f63a5d585d0a1a0db8cafa44c90e85bdf6c45197
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-14 17:31:17 +0500

    Stopwatch markup_49les

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,14 +1,20 @@
 <script>
-import { BUTTON_TYPE_PRIMARY, BUTTON_TYPE_DANGER, BUTTON_TYPE_NEUTRAL } from '../constants'
+import {
+  BUTTON_TYPE_PRIMARY,
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_DANGER,
+  BUTTON_TYPE_NEUTRAL
+} from '../constants'
 const typeClasses = {
   [BUTTON_TYPE_PRIMARY]: 'bg-purple-500 enabled:hover:bg-purple-600 text-white',
+  [BUTTON_TYPE_SUCCESS]: 'bg-green-500 enabled:hover:bg-green-600 text-white',
+  [BUTTON_TYPE_WARNING]: 'bg-yellow-500 enabled:hover:bg-yellow-600 text-white',
   [BUTTON_TYPE_DANGER]: 'bg-red-500 enabled:hover:bg-red-600 text-white',
   [BUTTON_TYPE_NEUTRAL]: 'bg-gray-100 enabled:hover:bg-gray-200'
 }
-  
 </script>
 <script setup>
-
 import { isButtonTypeValid } from '../validators'
 defineProps({
   type: {
@@ -17,12 +23,10 @@ defineProps({
     validator: isButtonTypeValid
   }
 })
-
 </script>
-
 <template>
   <button
-    :class="`${typeClasses[type]} rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
+      :class="`${typeClasses[type]} rounded p-3 disabled:cursor-not-allowed disabled:opacity-50`"
   >
     <slot></slot>
   </button>
--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { NULLABLE_ACTIVITY } from '../constants'
+import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
+import {
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_DANGER,
+  NULLABLE_ACTIVITY
+} from '../constants'
 import {
   isTimelineItemValid,
   isActivityValid,
   validateSelectOptions,
   validateActivities
 } from '../validators'
+import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
 import TimelineHour from './TimelineHour.vue'
 
@@ -30,13 +37,12 @@ import TimelineHour from './TimelineHour.vue'
     selectActivity: isActivityValid
   })
   function selectActivity(id) {
-  emit('selectActivity', findActivityById(id))
-}
-function findActivityById(id) {
-  return props.activities.find((activity) => activity.id === id) || NULLABLE_ACTIVITY
-}
+    emit('selectActivity', findActivityById(id))
+  }
+  function findActivityById(id) {
+    return props.activities.find((activity) => activity.id === id) || NULLABLE_ACTIVITY
+  }
 </script>
-
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
     <TimelineHour :hour="timelineItem.hour" />
@@ -46,5 +52,19 @@ function findActivityById(id) {
         :options="activitySelectOptions"
         @select="selectActivity"
     />
+    <div class="flex w-full gap-2">
+      <BaseButton :type="BUTTON_TYPE_DANGER">
+        <ArrowPathIcon class="h-8" />
+      </BaseButton>
+      <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
+        00:00:00
+      </div>
+      <BaseButton :type="BUTTON_TYPE_WARNING">
+        <PauseIcon class="h-8" />
+      </BaseButton>
+      <BaseButton :type="BUTTON_TYPE_SUCCESS">
+        <PlayIcon class="h-8" />
+      </BaseButton>
+    </div>
   </li>
 </template>
