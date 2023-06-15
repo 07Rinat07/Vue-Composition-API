5a61d53d0a242734fe42ca58a3cc1daa608c8a67
Rinat Sarmuldin <ura07srr@gmail.com>
2023-06-15 21:37:14 +0500

    Disable stopwatch buttons_53les

--- src/components/TimelineItem.vue
+++ src/components/TimelineItem.vue
@@ -9,33 +9,38 @@ import {
 import BaseSelect from './BaseSelect.vue'
 import TimelineHour from './TimelineHour.vue'
 import TimelineStopwatch from './TimelineStopwatch.vue'
+
 const props = defineProps({
   timelineItem: {
-      required: true,
-          type: Object,
-          validator: isTimelineItemValid
-    },
-    activities: {
-      required: true,
-      type: Array,
-      validator: validateActivities
-    },
-    activitySelectOptions: {
-      required: true,
-      type: Array,
-      validator: validateSelectOptions
-    }
-  })
+    required: true,
+    type: Object,
+    validator: isTimelineItemValid
+  },
+  activities: {
+    required: true,
+    type: Array,
+    validator: validateActivities
+  },
+  activitySelectOptions: {
+    required: true,
+    type: Array,
+    validator: validateSelectOptions
+  }
+})
+
 const emit = defineEmits({
   selectActivity: isActivityValid
 })
+
 function selectActivity(id) {
   emit('selectActivity', findActivityById(id))
 }
+
 function findActivityById(id) {
   return props.activities.find((activity) => activity.id === id) || NULLABLE_ACTIVITY
 }
 </script>
+
 <template>
   <li class="relative flex flex-col gap-2 border-t border-gray-200 py-10 px-4">
     <TimelineHour :hour="timelineItem.hour" />
@@ -45,6 +50,6 @@ function findActivityById(id) {
         :options="activitySelectOptions"
         @select="selectActivity"
     />
-    <TimelineStopwatch :seconds="timelineItem.activitySeconds" />
+    <TimelineStopwatch :seconds="timelineItem.activitySeconds" :hour="timelineItem.hour" />
   </li>
 </template>
