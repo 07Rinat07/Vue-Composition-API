4261db3a07f37bbd2342e038e7f59befcf944c09
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-07 12:56:08 +0500

    Update time of ActivitySecondsToComplete component_60_L

--- src/components/ActivitySecondsToComplete.vue
+++ src/components/ActivitySecondsToComplete.vue
@@ -1,25 +1,34 @@
 <script setup>
+import { computed } from 'vue'
 import { formatSeconds, getTotalActivitySeconds } from '../functions'
 import { isActivityValid, validateTimelineItems } from '../validators'
-const props = defineProps({
-  activity: {
-    required: true,
-    type: Object,
-    validator: isActivityValid
-  },
-  timelineItems: {
-    required: true,
-    type: Array,
-    validator: validateTimelineItems
-  }
-})
-const seconds = formatSeconds(
-    getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete
-)
+  const props = defineProps({
+    activity: {
+      required: true,
+      type: Object,
+      validator: isActivityValid
+    },
+    timelineItems: {
+      required: true,
+      type: Array,
+      validator: validateTimelineItems
+    }
+  })
+  const classes = computed(
+      () =>
+          `flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600 ${colorClasses.value}`
+  )
+  const colorClasses = computed(() =>
+      secondsDiff.value < 0 ? 'bg-red-100 text-red-600' : 'bg-green-100 text-green-600'
+  )
+  const seconds = computed(() => `${sign.value}${formatSeconds(secondsDiff.value)}`)
+  const sign = computed(() => (secondsDiff.value >= 0 ? '+' : '-'))
+  const secondsDiff = computed(
+      () =>
+          getTotalActivitySeconds(props.activity, props.timelineItems) - props.activity.secondsToComplete
+  )
 </script>
 
 <template>
-  <div class="flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600">
-    {{ seconds }}
-  </div>
-</template>>
+  <div :class="classes">{{ seconds }}</div>
+</template>
