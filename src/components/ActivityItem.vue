6f857de5a54ac203f170a02b9a7311338263d5a1
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-07 11:23:13 +0500

    ActivitySecondsToComplete component_58_L

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -4,21 +4,19 @@ import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '../constants'
 import { isActivityValid, isUndefined, isNumber } from '../validators'
 import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
-
+import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
 defineProps({
   activity: {
-    required: true,
-    type: Object,
-    validator: isActivityValid
-  }
-})
-
+      required: true,
+      type: Object,
+      validator: isActivityValid
+    }
+  })
 const emit = defineEmits({
   setSecondsToComplete: isNumber,
   delete: isUndefined
 })
 </script>
-
 <template>
   <li class="flex flex-col gap-2 p-4">
     <div class="flex items-center gap-2">
@@ -27,14 +25,15 @@ const emit = defineEmits({
       </BaseButton>
       <span class="truncate text-xl">{{ activity.name }}</span>
     </div>
-    <div>
+    <div class="flex gap-2">
       <BaseSelect
-        class="font-mono"
-        placeholder="hh:mm"
-        :options="PERIOD_SELECT_OPTIONS"
-        :selected="activity.secondsToComplete || null"
-        @select="emit('setSecondsToComplete', $event || 0)"
+          class="grow font-mono"
+          placeholder="hh:mm"
+          :options="PERIOD_SELECT_OPTIONS"
+          :selected="activity.secondsToComplete || null"
+          @select="emit('setSecondsToComplete', $event || 0)"
       />
+      <ActivitySecondsToComplete v-if="activity.secondsToComplete" :activity="activity" />
     </div>
   </li>
 </template>
