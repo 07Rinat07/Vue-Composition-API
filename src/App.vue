3d5ecf8363713abe94dd8c412873a06528b696e3
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-12 00:20:20 +0500

    Inject setActivitySecondsToComplete function into ActivityItem component_67__L

--- src/App.vue
+++ src/App.vue
@@ -13,23 +13,33 @@ import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
+
 const currentPage = ref(normalizePageHash())
+
 const activities = ref(generateActivities())
+
 const timelineItems = ref(generateTimelineItems(activities.value))
+
 const timeline = ref()
+
 const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
+
 function goTo(page) {
   if (currentPage.value === PAGE_TIMELINE && page === PAGE_TIMELINE) {
     timeline.value.scrollToHour()
   }
+
   if (page !== PAGE_TIMELINE) {
     document.body.scrollIntoView()
   }
+
   currentPage.value = page
 }
+
 function createActivity(activity) {
   activities.value.push(activity)
 }
+
 function deleteActivity(activity) {
   timelineItems.value.forEach((timelineItem) => {
     if (timelineItem.activityId === activity.id) {
@@ -37,28 +47,40 @@ function deleteActivity(activity) {
       timelineItem.activitySeconds = 0
     }
   })
+
   activities.value.splice(activities.value.indexOf(activity), 1)
 }
+
 function setTimelineItemActivity(timelineItem, activityId) {
   timelineItem.activityId = activityId
 }
-function updateTimelineItemActivitySeconds(timelineItem, activitySeconds) {
 
-    timelineItem.activitySeconds += activitySeconds
-  }
-  function setActivitySecondsToComplete(activity, secondsToComplete) {
-    activity.secondsToComplete = secondsToComplete
-  }
-  provide('updateTimelineItemActivitySeconds', updateTimelineItemActivitySeconds)
-  provide('setTimelineItemActivity', setTimelineItemActivity)
-  provide('activitySelectOptions', activitySelectOptions.value)
-  provide('periodSelectOptions', generatePeriodSelectOptions())
-  provide('timelineItems', timelineItems.value)
+function updateTimelineItemActivitySeconds(timelineItem, activitySeconds) {
+  timelineItem.activitySeconds += activitySeconds
+}
+
+function setActivitySecondsToComplete(activity, secondsToComplete) {
+  activity.secondsToComplete = secondsToComplete
+
+}
+
+
+
+provide('updateTimelineItemActivitySeconds', updateTimelineItemActivitySeconds)
+
+provide('setActivitySecondsToComplete', setActivitySecondsToComplete)
+
+provide('setTimelineItemActivity', setTimelineItemActivity)
+
+provide('activitySelectOptions', activitySelectOptions.value)
+
+provide('periodSelectOptions', generatePeriodSelectOptions())
+provide('timelineItems', timelineItems.value)
 </script>
 
 <template>
-
   <TheHeader @navigate="goTo($event)" />
+
   <main class="flex flex-grow flex-col">
     <TheTimeline
         v-show="currentPage === PAGE_TIMELINE"
@@ -68,12 +90,19 @@ function updateTimelineItemActivitySeconds(timelineItem, activitySeconds) {
     />
     <TheActivities
         v-show="currentPage === PAGE_ACTIVITIES"
+
         :activities="activities"
+
         @create-activity="createActivity"
+
         @delete-activity="deleteActivity"
-        @set-activity-seconds-to-complete="setActivitySecondsToComplete"
+
+
     />
+
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
+
   </main>
+
   <TheNav :current-page="currentPage" @navigate="goTo($event)" />
 </template>
--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -1,30 +1,31 @@
 <script setup>
-import { inject } from 'vue'
-import { TrashIcon } from '@heroicons/vue/24/outline'
-import { BUTTON_TYPE_DANGER } from '../constants'
-import { isActivityValid, isUndefined, isNumber } from '../validators'
+import {inject} from 'vue'
+import {TrashIcon} from '@heroicons/vue/24/outline'
+import {BUTTON_TYPE_DANGER} from '../constants'
+import {isActivityValid, isUndefined} from '../validators'
 import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
-  import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
-  defineProps({
-                activity: {
-                  required: true,
-                  type: Object,
-                  validator: isActivityValid
-                }
-              })
-  const emit = defineEmits({
-    setSecondsToComplete: isNumber,
-    delete: isUndefined
-  })
-  const periodSelectOptions = inject('periodSelectOptions')
+import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
+
+defineProps({
+  activity: {
+    required: true,
+    type: Object,
+    validator: isActivityValid
+  }
+})
+const emit = defineEmits({
+  delete: isUndefined
+})
+const setActivitySecondsToComplete = inject('setActivitySecondsToComplete')
+const periodSelectOptions = inject('periodSelectOptions')
 </script>
 
 <template>
   <li class="flex flex-col gap-2 p-4">
     <div class="flex items-center gap-2">
       <BaseButton :type="BUTTON_TYPE_DANGER" @click="emit('delete')">
-        <TrashIcon class="h-8" />
+        <TrashIcon class="h-8"/>
       </BaseButton>
       <span class="truncate text-xl">{{ activity.name }}</span>
     </div>
@@ -34,9 +35,9 @@ import BaseSelect from './BaseSelect.vue'
           placeholder="hh:mm"
           :options="periodSelectOptions"
           :selected="activity.secondsToComplete || null"
-          @select="emit('setSecondsToComplete', $event || 0)"
+          @select="setActivitySecondsToComplete(activity, $event || 0)"
       />
-      <ActivitySecondsToComplete v-if="activity.secondsToComplete" :activity="activity" />
+      <ActivitySecondsToComplete v-if="activity.secondsToComplete" :activity="activity"/>
     </div>
   </li>
 </template>
