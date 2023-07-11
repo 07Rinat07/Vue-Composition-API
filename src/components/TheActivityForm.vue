f638b0f0cb336e37ab55792abc16ab63aa598346
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-12 00:24:26 +0500

    Inject createActivity function into TheActivityForm component___68___L

--- src/components/TheActivityForm.vue
+++ src/components/TheActivityForm.vue
@@ -1,38 +1,28 @@
 <script setup>
-import { ref, nextTick } from 'vue'
+import { ref, inject, nextTick } from 'vue'
 import { PlusIcon } from '@heroicons/vue/24/outline'
 import { id } from '../functions'
-import { isActivityValid } from '../validators'
 import BaseButton from './BaseButton.vue'
-
-const emit = defineEmits({
-  submit: isActivityValid
-})
-
+const createActivity = inject('createActivity')
 const name = ref('')
-
 async function submit() {
-  emit('submit', {
+  createActivity({
     id: id(),
     name: name.value,
     secondsToComplete: 0
   })
-
   name.value = ''
-
   await nextTick()
-
   window.scrollTo(0, document.body.scrollHeight)
 }
 </script>
-
 <template>
   <form @submit.prevent="submit" class="sticky bottom-[57px] flex gap-2 border-t bg-white p-4">
     <input
-      type="text"
-      v-model="name"
-      class="w-full rounded border px-4 text-xl"
-      placeholder="Activity name"
+        type="text"
+        v-model="name"
+        class="w-full rounded border px-4 text-xl"
+        placeholder="Activity name"
     />
     <BaseButton :disabled="name.trim() === ''">
       <PlusIcon class="h-8" />
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,9 +1,8 @@
 <script setup>
-import {validateActivities, isActivityValid} from '../validators'
+import { validateActivities, isActivityValid } from '../validators'
 import ActivityItem from '../components/ActivityItem.vue'
 import TheActivityForm from '../components/TheActivityForm.vue'
 import TheActivitiesEmptyState from '../components/TheActivitiesEmptyState.vue'
-
 defineProps({
   activities: {
     required: true,
@@ -12,13 +11,10 @@ defineProps({
   }
 })
 const emit = defineEmits({
-  createActivity: isActivityValid,
   deleteActivity: isActivityValid
 })
 </script>
-
 <template>
-
   <div class="flex grow flex-col">
     <ul v-if="activities.length" class="grow divide-y">
       <ActivityItem
@@ -28,7 +24,7 @@ const emit = defineEmits({
           @delete="emit('deleteActivity', activity)"
       />
     </ul>
-    <TheActivitiesEmptyState v-else/>
-    <TheActivityForm @submit="emit('createActivity', $event)"/>
+    <TheActivitiesEmptyState v-else />
+    <TheActivityForm />
   </div>
 </template>
