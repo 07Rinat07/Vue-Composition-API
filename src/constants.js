f59ad65b92a553b20fcaa284afb79613c3100627
Rinat Sarmuldin <ura07srr@gmail.com>
2023-07-25 22:16:33 +0500

    Change nav items representation__74__L_

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,22 @@
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
-
 export const PAGE_TIMELINE = 'timeline'
 export const PAGE_ACTIVITIES = 'activities'
 export const PAGE_PROGRESS = 'progress'
 
-export const NAV_ITEMS = {
-  [PAGE_TIMELINE]: ClockIcon,
-  [PAGE_ACTIVITIES]: ListBulletIcon,
-  [PAGE_PROGRESS]: ChartBarIcon
-}
+export const NAV_ITEMS = [
+  {
+    page: PAGE_TIMELINE,
+    icon: ClockIcon
+  },
+  {
+    page: PAGE_ACTIVITIES,
+    icon: ListBulletIcon
+  },
+  {
+    page: PAGE_PROGRESS,
+    icon: ChartBarIcon
+  }
+]
 
 export const BUTTON_TYPE_PRIMARY = 'primary'
 export const BUTTON_TYPE_SUCCESS = 'success'
@@ -22,7 +30,6 @@ export const BUTTON_TYPES = [
   BUTTON_TYPE_NEUTRAL,
   BUTTON_TYPE_DANGER
 ]
-
 export const MILLISECONDS_IN_SECOND = 1000
 export const SECONDS_IN_MINUTE = 60
 export const MINUTES_IN_HOUR = 60
