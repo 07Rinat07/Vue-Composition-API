4bdbe4a7cf4589a543a4632d938be35989f1614d
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-19 01:17:32 +0500

    Create generic function to update activities__86__L

--- src/activities.js
+++ src/activities.js
@@ -1,27 +1,27 @@
 import { ref, computed } from 'vue'
 import { SECONDS_IN_HOUR } from './constants'
 import { id } from './functions'
-
 export const activities = ref(generateActivities())
-    export const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
-    export function createActivity(activity) {
-        activities.value.push(activity)
-    }
-    export function deleteActivity(activity) {
-        activities.value.splice(activities.value.indexOf(activity), 1)
-    }
-    export function setActivitySecondsToComplete(activity, secondsToComplete) {
-        activity.secondsToComplete = secondsToComplete || 0
-    }
+export const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
+export function createActivity(activity) {
+    activities.value.push(activity)
+}
 
-    function generateActivities() {
-        return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
-            id: id(),
-            name,
-            secondsToComplete: hours * SECONDS_IN_HOUR
-        }))
-    }
+export function updateActivity(activity, fields) {
+    return Object.assign(activity, fields)
+}
 
-    function generateActivitySelectOptions(activities) {
-        return activities.map((activity) => ({ value: activity.id, label: activity.name }))
-    }
+export function deleteActivity(activity) {
+    activities.value.splice(activities.value.indexOf(activity), 1)
+}
+
+function generateActivities() {
+    return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
+        id: id(),
+        name,
+        secondsToComplete: hours * SECONDS_IN_HOUR
+    }))
+}
+function generateActivitySelectOptions(activities) {
+    return activities.map((activity) => ({ value: activity.id, label: activity.name }))
+}
