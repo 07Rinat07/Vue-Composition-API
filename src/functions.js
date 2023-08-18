a4ecdc0b60ae9be296e0cc48e5bbe1a6c1b56201
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-19 00:45:50 +0500

    Refactor activities module__81_L

--- src/functions.js
+++ src/functions.js
@@ -1,11 +1,7 @@
-import {
-  MILLISECONDS_IN_SECOND,
-  SECONDS_IN_HOUR,
-  SECONDS_IN_MINUTE,
-  MINUTES_IN_HOUR
-} from './constants'
+import { MILLISECONDS_IN_SECOND, SECONDS_IN_MINUTE, MINUTES_IN_HOUR } from './constants'
 import { isNull } from './validators'
-  export function currentHour() {
+
+export function currentHour() {
     return new Date().getHours()
   }
   export function formatSeconds(seconds) {
@@ -17,37 +13,28 @@ import { isNull } from './validators'
   export function normalizeSelectValue(value) {
     return isNull(value) || isNaN(value) ? value : +value
   }
-  export function generateActivities() {
-    return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
-      id: id(),
-      name,
-      secondsToComplete: hours * SECONDS_IN_HOUR
-    }))
-  }
+
   export function id() {
     return Date.now().toString(36) + Math.random().toString(36).substring(2)
   }
-  export function getTotalActivitySeconds(activity, timelineItems) {
-    return timelineItems
-        .filter((timelineItem) => timelineItem.activityId === activity.id)
-        .reduce((totalSeconds, timelineItem) => Math.round(timelineItem.activitySeconds + totalSeconds), 0)
-  }
 
-  export function generateActivitySelectOptions(activities) {
-    return activities.map((activity) => ({ value: activity.id, label: activity.name }))
-  }
+    export function getTotalActivitySeconds(activity, timelineItems) {
+      return timelineItems
+          .filter((timelineItem) => timelineItem.activityId === activity.id)
+          .reduce((totalSeconds, timelineItem) => Math.round(timelineItem.activitySeconds + totalSeconds), 0)
+    }
 
-  export function generatePeriodSelectOptions() {
-    const periodsInMinutes = [
-      15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480
-    ]
-    return periodsInMinutes.map((periodInMinutes) => ({
-      value: periodInMinutes * SECONDS_IN_MINUTE,
-      label: generatePeriodSelectOptionsLabel(periodInMinutes)
-    }))
-  }
-  function generatePeriodSelectOptionsLabel(periodInMinutes) {
-    const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR).toString().padStart(2, 0)
-    const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
-    return `${hours}:${minutes}`
-  }
+    export function generatePeriodSelectOptions() {
+      const periodsInMinutes = [
+        15, 30, 45, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360, 390, 420, 450, 480
+      ]
+      return periodsInMinutes.map((periodInMinutes) => ({
+        value: periodInMinutes * SECONDS_IN_MINUTE,
+        label: generatePeriodSelectOptionsLabel(periodInMinutes)
+      }))
+    }
+    function generatePeriodSelectOptionsLabel(periodInMinutes) {
+      const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR).toString().padStart(2, 0)
+      const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
+      return `${hours}:${minutes}`
+    }
