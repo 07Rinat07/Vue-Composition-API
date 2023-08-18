eed9c1c22b9ad76c7fc1aacad7cf288c1826c53b
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-19 01:12:26 +0500

    Create generic function to update timeline items__85_L

--- src/components/TimelineStopwatch.vue
+++ src/components/TimelineStopwatch.vue
@@ -1,16 +1,17 @@
 <script setup>
-import { ref, watch } from 'vue'
-import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/vue/24/outline'
+import {ref, watch} from 'vue'
+import {ArrowPathIcon, PauseIcon, PlayIcon} from '@heroicons/vue/24/outline'
 import {
   MILLISECONDS_IN_SECOND,
-BUTTON_TYPE_SUCCESS,
-    BUTTON_TYPE_WARNING,
-    BUTTON_TYPE_DANGER
+  BUTTON_TYPE_SUCCESS,
+  BUTTON_TYPE_WARNING,
+  BUTTON_TYPE_DANGER
 } from '../constants'
-import { currentHour, formatSeconds } from '../functions'
-import { isTimelineItemValid } from '../validators'
-import { updateTimelineItemActivitySeconds } from '../timeline-items'
+import {currentHour, formatSeconds} from '../functions'
+import {isTimelineItemValid} from '../validators'
+import {updateTimelineItem} from '../timeline-items'
 import BaseButton from './BaseButton.vue'
+
 const props = defineProps({
   timelineItem: {
     required: true,
@@ -23,41 +24,45 @@ const isRunning = ref(false)
 const isStartButtonDisabled = props.timelineItem.hour !== currentHour()
 watch(
     () => props.timelineItem.activityId,
-    () => updateTimelineItemActivitySeconds(props.timelineItem, seconds.value)
+    () => updateTimelineItem(props.timelineItem, {activitySeconds: seconds.value})
 )
+
 function start() {
   isRunning.value = setInterval(() => {
-    updateTimelineItemActivitySeconds(props.timelineItem, props.timelineItem.activitySeconds + 1)
+    updateTimelineItem(props.timelineItem, {
+      activitySeconds: props.timelineItem.activitySeconds + 1
+    })
     seconds.value++
   }, MILLISECONDS_IN_SECOND)
+}
 
-  }
-  function stop() {
-    clearInterval(isRunning.value)
-    isRunning.value = false
-  }
-  function reset() {
-    stop()
-    updateTimelineItemActivitySeconds(
-        props.timelineItem,
-        props.timelineItem.activitySeconds - seconds.value
-    )
-    seconds.value = 0
-  }
+function stop() {
+  clearInterval(isRunning.value)
+  isRunning.value = false
+}
+
+function reset() {
+  stop()
+  updateTimelineItem(props.timelineItem, {
+    activitySeconds: props.timelineItem.activitySeconds - seconds.value
+  })
+  seconds.value = 0
+}
+
 </script>
 <template>
   <div class="flex w-full gap-2">
     <BaseButton :type="BUTTON_TYPE_DANGER" :disabled="!seconds" @click="reset">
-      <ArrowPathIcon class="h-8" />
+      <ArrowPathIcon class="h-8"/>
     </BaseButton>
     <div class="flex flex-grow items-center rounded bg-gray-100 px-2 font-mono text-3xl">
       {{ formatSeconds(seconds) }}
     </div>
     <BaseButton v-if="isRunning" :type="BUTTON_TYPE_WARNING" @click="stop">
-      <PauseIcon class="h-8" />
+      <PauseIcon class="h-8"/>
     </BaseButton>
     <BaseButton v-else :type="BUTTON_TYPE_SUCCESS" :disabled="isStartButtonDisabled" @click="start">
-      <PlayIcon class="h-8" />
+      <PlayIcon class="h-8"/>
     </BaseButton>
   </div>
 </template>
