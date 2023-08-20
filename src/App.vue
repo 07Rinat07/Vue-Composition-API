3a3c0b6b2c6244b89ca02e0b8592e4956de5acae
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-20 21:59:14 +0500

    Refactor scrolling to hours__87_L

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from './constants'
-import { currentPage, timelineRef } from './router'
+import { currentPage } from './router'
 import TheHeader from './components/TheHeader.vue'
 import TheNav from './components/TheNav.vue'
 import TheTimeline from './pages/TheTimeline.vue'
 import TheActivities from './pages/TheActivities.vue'
 import TheProgress from './pages/TheProgress.vue'
 </script>
-
 <template>
   <TheHeader />
+
   <main class="flex flex-grow flex-col">
-    <TheTimeline v-show="currentPage === PAGE_TIMELINE" ref="timelineRef" />
+    <TheTimeline v-show="currentPage === PAGE_TIMELINE" />
     <TheActivities v-show="currentPage === PAGE_ACTIVITIES" />
     <TheProgress v-show="currentPage === PAGE_PROGRESS" />
   </main>
--- src/pages/TheTimeline.vue
+++ src/pages/TheTimeline.vue
@@ -1,25 +1,17 @@
 <script setup>
-import { ref, watchPostEffect, nextTick } from 'vue'
-import { PAGE_TIMELINE, MIDNIGHT_HOUR } from '../constants'
-import { currentHour } from '../functions'
+import { watchPostEffect, nextTick } from 'vue'
+import { PAGE_TIMELINE } from '../constants'
 import { currentPage } from '../router'
-import { timelineItems } from '../timeline-items'
+import { timelineItems, timelineItemRefs, scrollToHour } from '../timeline-items'
 import TimelineItem from '../components/TimelineItem.vue'
-defineExpose({ scrollToHour })
-const timelineItemRefs = ref([])
-
 watchPostEffect(async () => {
   if (currentPage.value === PAGE_TIMELINE) {
     await nextTick()
     scrollToHour(null, false)
   }
 })
-function scrollToHour(hour = null, isSmooth = true) {
-  hour ??= currentHour()
-  const el = hour === MIDNIGHT_HOUR ? document.body : timelineItemRefs.value[hour - 1].$el
-  el.scrollIntoView({ behavior: isSmooth ? 'smooth' : 'instant' })
-}
 </script>
+
 <template>
   <div class="mt-7">
     <ul>
