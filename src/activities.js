a4dfa8e4e62c1a1018cfd7cc1d370c3c7a89072f
Rinat Sarmuldin <ura07srr@gmail.com>
2023-08-21 12:18:37 +0500

    Progress item component__95_L_

--- src/activities.js
+++ src/activities.js
@@ -1,20 +1,25 @@
 import { ref, computed } from 'vue'
 import { SECONDS_IN_HOUR } from './constants'
 import { id } from './functions'
+
 export const activities = ref(generateActivities())
+
+export const trackedActivities = computed(() =>
+    activities.value.filter(({ secondsToComplete }) => secondsToComplete)
+)
+
 export const activitySelectOptions = computed(() => generateActivitySelectOptions(activities.value))
+
 export function createActivity(activity) {
+
     activities.value.push(activity)
 }
-
 export function updateActivity(activity, fields) {
     return Object.assign(activity, fields)
 }
-
 export function deleteActivity(activity) {
     activities.value.splice(activities.value.indexOf(activity), 1)
 }
-
 function generateActivities() {
     return ['Coding', 'Reading', 'Training'].map((name, hours) => ({
         id: id(),
