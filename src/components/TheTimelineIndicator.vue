f7a425d783701ab28a2986dc5093ec562ee5c73f
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-06 17:26:11 +0500

    Dynamically change indicator position___111

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,29 +1,42 @@
 <script setup>
-import { ref } from 'vue'
-import { HUNDRED_PERCENT, SECONDS_IN_DAY, SECONDS_IN_MINUTE, MINUTES_IN_HOUR } from '../constants'
+import { ref, computed, watchEffect } from 'vue'
+import {
+  HUNDRED_PERCENT,
+  SECONDS_IN_DAY,
+  SECONDS_IN_MINUTE,
+  MINUTES_IN_HOUR,
+  MILLISECONDS_IN_SECOND
+} from '@/constants'
+const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 const indicatorRef = ref()
-function calculateTopOffset() {
-  return (calculateSecondsSinceMidnightInPercentage() * getTimelineHeight()) / HUNDRED_PERCENT
-}
-function calculateSecondsSinceMidnightInPercentage() {
-  return (HUNDRED_PERCENT * calculateSecondsSinceMidnight()) / SECONDS_IN_DAY
-}
+setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECOND)
+const topOffset = computed(
+    () => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT
+)
+const secondsSinceMidnightInPercentage = computed(
+    () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+)
+watchEffect(() => {
+  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
+    secondsSinceMidnight.value = 0
+  }
+})
 function calculateSecondsSinceMidnight() {
   const now = new Date()
-  return (
-      SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-      SECONDS_IN_MINUTE * now.getMinutes() +
-      now.getSeconds()
-  )
-}
-function getTimelineHeight() {
-  return indicatorRef.value?.parentNode.getBoundingClientRect().height
-}
+    return (
+        SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
+        SECONDS_IN_MINUTE * now.getMinutes() +
+        now.getSeconds()
+    )
+  }
+  function getTimelineHeight() {
+    return indicatorRef.value?.parentNode.getBoundingClientRect().height
+  }
 </script>
 <template>
   <hr
       ref="indicatorRef"
       class="pointer-events-none absolute z-10 w-full border-b-2 border-red-600"
-      :style="{ top: `${calculateTopOffset()}px` }"
+      :style="{ top: `${topOffset}px` }"
   />
 </template>
