a337abd96ebc737d5e87e8a6edb6ebee2a3b9ae3
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-07 11:12:14 +0500

    Refactor global time module __115

--- src/time.js
+++ src/time.js
@@ -1,36 +1,26 @@
-import { ref, computed, watchEffect } from 'vue'
-import {
-HUNDRED_PERCENT,
-SECONDS_IN_DAY,
-SECONDS_IN_MINUTE,
-MINUTES_IN_HOUR,
-MILLISECONDS_IN_SECOND
-} from './constants'
+import { ref, computed } from 'vue'
+import { HUNDRED_PERCENT, SECONDS_IN_DAY, MILLISECONDS_IN_SECOND } from './constants'
 
-function calculateSecondsSinceMidnight() {
-const now = new Date()
-return (
-SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-SECONDS_IN_MINUTE * now.getMinutes() +
-now.getSeconds()
-)
-}
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
+export const now = ref(new Date())
+
 export const secondsSinceMidnightInPercentage = computed(
-() => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+    () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
 )
-watchEffect(() => {
-if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-secondsSinceMidnight.value = 0
-}
-})
+
+const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
+
+const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECOND)
+
 let timer = null
+
 export function startTimer() {
-secondsSinceMidnight.value = calculateSecondsSinceMidnight()
-timer = setInterval(() => {
-secondsSinceMidnight.value += 5 * 60
-}, MILLISECONDS_IN_SECOND)
+    now.value = new Date()
+
+    timer = setInterval(() => {
+        now.value = new Date(now.value.getTime() + 5 * 60 * MILLISECONDS_IN_SECOND)
+    }, MILLISECONDS_IN_SECOND)
 }
+
 export function stopTimer() {
-clearInterval(timer)
+    clearInterval(timer)
 }
