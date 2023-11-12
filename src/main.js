0c6bd2d6f9e9d4516160d515fa52935467798c9a
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-12 19:47:52 +0500

    Reset timeline items if state is outdated__120

--- src/main.js
+++ src/main.js
@@ -2,27 +2,24 @@ import { createApp } from 'vue'
 import * as storage from './storage'
 import { activities } from './activities'
 import { timelineItems } from './timeline-items'
+import { isToday } from './time'
 import App from './App.vue'
 
 import './assets/main.css'
-
-loadState()
-
-document.addEventListener('visibilitychange', () => {
-    document.visibilityState === 'visible' ? loadState() : saveState()
-})
-
-function loadState() {
-    const state = storage.load()
-
-    timelineItems.value = state.timelineItems
-    activities.value = state.activities
-}
-
-function saveState() {
-    storage.save({
-        timelineItems: timelineItems.value,
-        activities: activities.value
+    loadState()
+    document.addEventListener('visibilitychange', () => {
+        document.visibilityState === 'visible' ? loadState() : saveState()
     })
-}
-createApp(App).mount('#app')
+    function loadState() {
+        const state = storage.load()
+
+        timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
+        activities.value = state.activities || activities.value
+    }
+    function saveState() {
+        storage.save({
+            timelineItems: timelineItems.value,
+            activities: activities.value
+        })
+    }
+    createApp(App).mount('#app')
