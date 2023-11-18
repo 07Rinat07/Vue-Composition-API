e669fcbdfaadf861bf4589400b38cc416c49943f
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-19 00:34:54 +0500

    Sync idle seconds__124

--- src/storage.js
+++ src/storage.js
@@ -1,24 +1,37 @@
-import { APP_NAME } from './constants'
-import { isToday, today } from './time'
-import { timelineItems } from './timeline-items'
-import { activities } from './activities'
+import {APP_NAME, MILLISECONDS_IN_SECOND} from './constants'
+import {isToday, today} from './time'
+import {timelineItems} from './timeline-items'
+import {activities} from './activities'
 
 export function loadState() {
-    const serializedState = localStorage.getItem(APP_NAME)
+        const serializedState = localStorage.getItem(APP_NAME)
 
-    const state = serializedState ? JSON.parse(serializedState) : {}
+        const state = serializedState ? JSON.parse(serializedState) : {}
 
-    timelineItems.value = isToday(new Date(state.date)) ? state.timelineItems : timelineItems.value
-    activities.value = state.activities || activities.value
-}
+        activities.value = state.activities || activities.value
 
-export function saveState() {
-    localStorage.setItem(
-        APP_NAME,
-        JSON.stringify({
-            timelineItems: timelineItems.value,
-            activities: activities.value,
-            date: today()
-        })
-    )
-}
+        const lastActiveAt = new Date(state.lastActiveAt)
+
+        timelineItems.value = isToday(lastActiveAt)
+            ? syncIdleSeconds(state.timelineItems, lastActiveAt)
+            : timelineItems.value
+    }
+    export function saveState() {
+            localStorage.setItem(
+                APP_NAME,
+                JSON.stringify({
+                    timelineItems: timelineItems.value,
+                    activities: activities.value,
+                    lastActiveAt: today()
+                })
+            )
+        }
+        function syncIdleSeconds(timelineItems, lastActiveAt) {
+            const activeTimelineItem = timelineItems.find(({ isActive }) => isActive)
+
+            if (activeTimelineItem) {
+                activeTimelineItem.activitySeconds += (today() - lastActiveAt) / MILLISECONDS_IN_SECOND
+            }
+
+            return timelineItems
+        }
