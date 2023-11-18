6235a038fc002b90f71678233832e874459b8848
Rinat Sarmuldin <ura07srr@gmail.com>
2023-11-19 00:40:08 +0500

    Calculate idle seconds_125

--- src/storage.js
+++ src/storage.js
@@ -1,37 +1,49 @@
-import {APP_NAME, MILLISECONDS_IN_SECOND} from './constants'
-import {isToday, today} from './time'
-import {timelineItems} from './timeline-items'
-import {activities} from './activities'
-
-export function loadState() {
+import { APP_NAME, MILLISECONDS_IN_SECOND, SECONDS_IN_HOUR } from './constants'
+import { isToday, today } from './time'
+import { timelineItems } from './timeline-items'
+import { activities } from './activities'
+    export function loadState() {
         const serializedState = localStorage.getItem(APP_NAME)
-
         const state = serializedState ? JSON.parse(serializedState) : {}
-
         activities.value = state.activities || activities.value
-
         const lastActiveAt = new Date(state.lastActiveAt)
-
         timelineItems.value = isToday(lastActiveAt)
             ? syncIdleSeconds(state.timelineItems, lastActiveAt)
             : timelineItems.value
     }
     export function saveState() {
-            localStorage.setItem(
-                APP_NAME,
-                JSON.stringify({
-                    timelineItems: timelineItems.value,
-                    activities: activities.value,
-                    lastActiveAt: today()
-                })
-            )
-        }
-        function syncIdleSeconds(timelineItems, lastActiveAt) {
-            const activeTimelineItem = timelineItems.find(({ isActive }) => isActive)
+        localStorage.setItem(
+            APP_NAME,
+            JSON.stringify({
+                timelineItems: timelineItems.value,
+                activities: activities.value,
+                lastActiveAt: today()
+            })
+        )
+    }
+    function syncIdleSeconds(timelineItems, lastActiveAt) {
+        const activeTimelineItem = timelineItems.find(({ isActive }) => isActive)
 
-            if (activeTimelineItem) {
-                activeTimelineItem.activitySeconds += (today() - lastActiveAt) / MILLISECONDS_IN_SECOND
-            }
-
-            return timelineItems
+        if (activeTimelineItem) {
+            activeTimelineItem.activitySeconds += calculateIdleSeconds(lastActiveAt)
         }
+
+        return timelineItems
+    }
+    function calculateIdleSeconds(lastActiveAt) {
+        let idleMilliseconds = today() - lastActiveAt
+
+        if (lastActiveAt.getHours() !== today().getHours()) {
+            idleMilliseconds = getEndOfIdleHour(lastActiveAt) - lastActiveAt
+        }
+        return idleMilliseconds / MILLISECONDS_IN_SECOND
+    }
+    function getEndOfIdleHour(lastActiveAt) {
+        const endOfIdleHour = new Date(lastActiveAt)
+
+        endOfIdleHour.setTime(endOfIdleHour.getTime() + SECONDS_IN_HOUR * MILLISECONDS_IN_SECOND)
+
+        endOfIdleHour.setMinutes(0, 0, 0)
+
+        return endOfIdleHour
+    }
--- src/time.js
+++ src/time.js
@@ -1,46 +1,35 @@
 import { ref, computed } from 'vue'
-import {
-    HUNDRED_PERCENT,
-    SECONDS_IN_DAY,
-    MILLISECONDS_IN_SECOND,
-    SECONDS_IN_MINUTE
-} from './constants'
+import { HUNDRED_PERCENT, SECONDS_IN_DAY, MILLISECONDS_IN_SECOND } from './constants'
 
 export function today() {
     const today = new Date()
 
-    today.setHours(0, 0)
+    // today.setHours(0, 0)
 
     return today
 }
+    export function tomorrow() {
+        const tomorrow = today()
+        tomorrow.setDate(tomorrow.getDate() + 1)
+        return tomorrow
+    }
+    export function isToday(date) {
+        return date.toDateString() === today().toDateString()
+    }
+    export const now = ref(today())
+    export const secondsSinceMidnightInPercentage = computed(
+        () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
+    )
+    const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
+    const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECOND)
+    let timer = null
+    export function startTimer() {
+        now.value = today()
 
-export function tomorrow() {
-    const tomorrow = today()
-
-    tomorrow.setDate(tomorrow.getDate() + 1)
-
-    return tomorrow
-}
-
-export function isToday(date) {
-    return date.toDateString() === today().toDateString()
-}
-
-export const now = ref(today())
-
-export const secondsSinceMidnightInPercentage = computed(
-    () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
-)
-const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
-const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECOND)
-let timer = null
-export function startTimer() {
-    now.value = today()
-
-    timer = setInterval(() => {
-        now.value = new Date(now.value.getTime() + SECONDS_IN_MINUTE * MILLISECONDS_IN_SECOND)
-    }, MILLISECONDS_IN_SECOND)
-}
-export function stopTimer() {
-    clearInterval(timer)
-}
+        timer = setInterval(() => {
+            now.value = new Date(now.value.getTime() + MILLISECONDS_IN_SECOND)
+        }, MILLISECONDS_IN_SECOND)
+    }
+    export function stopTimer() {
+        clearInterval(timer)
+    }
