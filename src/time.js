b38ecce1fb5a0406e0ad4e22c87a990f9c285615
Rinat Sarmuldin <ura07srr@gmail.com>
2023-12-04 12:48:50 +0500

    Stop and reset timers when new day begins_(final)136

--- src/time.js
+++ src/time.js
@@ -5,25 +5,15 @@ import {
     MILLISECONDS_IN_SECOND,
     SECONDS_IN_HOUR
 } from './constants'
-
 export const now = ref(today())
-
 export const secondsSinceMidnightInPercentage = computed(
     () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY
 )
-
 const secondsSinceMidnight = computed(() => (now.value - midnight.value) / MILLISECONDS_IN_SECOND)
-
 const midnight = computed(() => new Date(now.value).setHours(0, 0, 0, 0))
 
 export function today() {
-    const today = new Date()
-
-    // today.setHours(11)
-    // today.setMinutes(59)
-    // today.setSeconds(55)
-
-    return today
+    return new Date()
 }
 
 export function tomorrow() {
@@ -45,8 +35,5 @@ export function tomorrow() {
     }
 
     export function startCurrentDateTimer() {
-        // currentDateTimer = setInterval(() => (now.value = today()), MILLISECONDS_IN_SECOND)
-        setInterval(() => {
-            now.value = new Date(now.value.getTime() + MILLISECONDS_IN_SECOND)
-        }, MILLISECONDS_IN_SECOND)
+        setInterval(() => (now.value = today()), MILLISECONDS_IN_SECOND)
     }
--- src/timeline-item-timer.js
+++ src/timeline-item-timer.js
@@ -1,36 +1,27 @@
-import { ref, watchEffect } from 'vue'
+import { ref } from 'vue'
 import { MILLISECONDS_IN_SECOND } from './constants'
 import { activeTimelineItem, updateTimelineItem } from './timeline-items'
-import { now } from './time'
 
-export const timelineItemTimer = ref(false)
+const timelineItemTimer = ref(false)
 
 export function startTimelineItemTimer(timelineItem) {
-updateTimelineItem(timelineItem, { isActive: true })
+    timelineItem = timelineItem ?? activeTimelineItem.value
 
-timelineItemTimer.value = setInterval(() => {
-updateTimelineItem(timelineItem, {
-activitySeconds: timelineItem.activitySeconds + 1
-})
-}, MILLISECONDS_IN_SECOND)
+    updateTimelineItem(timelineItem, { isActive: true })
+    timelineItemTimer.value = setInterval(() => {
+        updateTimelineItem(timelineItem, {
+            activitySeconds: timelineItem.activitySeconds + 1
+        })
+    }, MILLISECONDS_IN_SECOND)
 }
-
-export function stopTimelineItemTimer(timelineItem) {
-updateTimelineItem(timelineItem, { isActive: false })
-
-clearInterval(timelineItemTimer.value)
-
-timelineItemTimer.value = false
+export function stopTimelineItemTimer() {
+    updateTimelineItem(activeTimelineItem.value, { isActive: false })
+    clearInterval(timelineItemTimer.value)
+    timelineItemTimer.value = false
 }
-
 export function resetTimelineItemTimer(timelineItem) {
-updateTimelineItem(timelineItem, { activitySeconds: 0 })
-
-stopTimelineItemTimer(timelineItem)
+    updateTimelineItem(timelineItem, { activitySeconds: 0 })
+    if (activeTimelineItem.value) {
+        stopTimelineItemTimer()
+    }
 }
-
-watchEffect(() => {
-if (activeTimelineItem.value && activeTimelineItem.value.hour !== now.value.getHours()) {
-stopTimelineItemTimer(activeTimelineItem.value)
-}
-})
